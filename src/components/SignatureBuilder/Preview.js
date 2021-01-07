cd00bd125feedf5765f04ba651ebe6aee169c9cc
Gustavo Siqueira <gjpsiqueira@gmail.com>
2021-01-07 19:25:06 -0300

    Add preview component

---
+++ src/components/SignatureBuilder/Preview.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import { Template2 } from '../Templates'
+
+const Preview = () => {
+    
+
+
+    return (
+        <Fragment>
+          <StyledPreview><Template2 /></StyledPreview>
+        </Fragment>
+    )
+}
+
+
+const StyledPreview = styled.div`
+    height:100vh;
+    position:relative;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    padding-left:20%;
+    z-index: -1; 
+}
+`
+export default Preview
