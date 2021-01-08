662e46d296e71c43e2cb56d03b2768105976cd54
Gustavo Siqueira <gjpsiqueira@gmail.com>
2021-01-08 15:35:30 -0300

    Add select index

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,8 +1,10 @@
 import InputText from './InputText'
+import Select from './Select'
 import { PrimaryButton, SecondaryButton } from './Button'
 
 export {
     InputText,
+    Select,
     PrimaryButton,
     SecondaryButton
 }
