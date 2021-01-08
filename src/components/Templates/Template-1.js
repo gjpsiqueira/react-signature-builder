bceba434ed16c323e80f39a1431010ba23ff9caa
Gustavo Siqueira <gjpsiqueira@gmail.com>
2021-01-08 17:28:45 -0300

    Add template example

---
+++ src/components/Templates/Template-1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Template1 = () => {
+
+	return null
+}
+
+export default Template1
---
+++ src/components/Templates/index.js
@@ -0,0 +1,5 @@
+import Template2 from './Template-2'
+
+export {
+    Template2
+}
