ee17fb27fe7459148415332a7e31cfa2f05d54d5
Gustavo Siqueira <gjpsiqueira@gmail.com>
2021-01-08 17:28:27 -0300

    Add unidade e events onBlur

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import { InputText, PrimaryButton, SecondaryButton } from '../Common'
+import { InputText, Select, PrimaryButton, SecondaryButton } from '../Common'
 import { useInputValue } from '../Contexts/InputContext'
 import * as S from './styled'
 
+const unidades = [
+    { label:'Barbacena' },
+    { label:'Lafaiete' },
+    { label:'Sabará' },
+    { label:'FUNJOBE' },
+    { label:'FUPAC SJN' },
+    { label:'FUPAC Lambari' },
+    { label:'FUPAC Baependi' },
+    { label:'FUPAC Visconde do Rio Branco' },
+    { label:'UNIPAC ITABIRITO' },
+
+]
 const Sidebar = () => {
     const { data, updateData } = useInputValue()
     return (
@@ -11,9 +23,16 @@ const Sidebar = () => {
                 <S.Title>Insira suas informações para assinatura </S.Title>
             </S.Container>
             <S.ContainerForm>
+                <InputText name="nome" value={data.nome} label="Nome" onBlur={updateData} />
                 <InputText name="email" value={data.email} label="Email" onBlur={updateData} />
-                <InputText name="Telefone" label="Telefone" />
-                <InputText name="Setor" label="Setor" />
+                <InputText name="telefone" value={data.telefone} label="Telefone" onBlur={updateData} />
+                <InputText name="setor" value={data.setor} label="Setor" onBlur={updateData} />
+                <Select name="unidade" onChange={updateData}>
+                    {unidades.map((item,index) => (
+                        <option value={index} key={index}>{item.label}</option>
+                    ))}
+                </Select>
+                <div style={{marginTop:'80px'}} />
                 <PrimaryButton>salvar assinatura</PrimaryButton>
                 <SecondaryButton>orientações</SecondaryButton>
             </S.ContainerForm>
