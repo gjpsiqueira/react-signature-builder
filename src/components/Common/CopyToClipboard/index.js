32852992ee51f60d64080498699a0a2e7975d8cc
Gustavo Siqueira <gjpsiqueira@gmail.com>
2021-01-08 20:46:36 -0300

    Add copyToCliboard function

---
+++ src/components/Common/CopyToClipboard/index.js
@@ -0,0 +1,15 @@
+function copyToClipboard(id: string) {
+	const targetId = document.getElementById(id)
+	if (targetId !== null) {
+		const markup = targetId.innerHTML
+		const tempInput = document.createElement('input')
+		const body = document.getElementsByTagName('body')[0]
+		body.appendChild(tempInput)
+		tempInput.setAttribute('value', markup)
+		tempInput.select()
+		document.execCommand('copy')
+		body.removeChild(tempInput)
+	}
+}
+
+export default copyToClipboard
